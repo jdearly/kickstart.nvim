c318b00bf51aac1674a3db41ffffe462a492bb24
Josh Early <josh.early@protonmail.com>
2025-04-12 18:44:41 -0400

    feat: colorscheme changes

--- init.lua
+++ init.lua
@@ -782,17 +782,18 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    -- vim.cmd.colorscheme 'tokyonight-night'
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'kanagawa-dragon'
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none',
-    vim.cmd.hi 'Normal ctermbg=none guibg=none',
-    -- end,
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'Normal ctermbg=none guibg=none'
+    end,
   },
 
   -- Highlight todo, notes, etc in comments
